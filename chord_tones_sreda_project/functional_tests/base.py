e1ad1829760575add28190066ee77438980dae03
m_sreda <mitya.sreda@gmail.com>
2024-08-26 19:44:58 +0300

    changed ci file and functional test

--- chord_tones_sreda_project/functional_tests/base.py
+++ chord_tones_sreda_project/functional_tests/base.py
@@ -1,17 +1,30 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
+class FunctionalTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.options = Options()
+        self.options.add_argument('--headless')
+        self.options.add_argument('--no-sandbox')
+        self.options.add_argument('--disable-dev-shm-usage')
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # Run Chrome in headless mode
-chrome_options.add_argument("--no-sandbox")  # Bypass OS security model, required for running in CI
-chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+        self.browser = webdriver.Chrome(options=self.options)
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')     
+    def tearDown(self) -> None:
+        self.browser.quit()
 
-body = browser.find_element(By.TAG_NAME, 'body')
-page_text = body.text
 
-assert 'Test view' in page_text
+    def test_starting_page(self):
+
+        self.browser.get('http://localhost:8000')
+
+        body = self.browser.find_element(By.TAG_NAME, 'body')
+        page_text = body.text
+
+        self.assertAlmostEqual('Test view',page_text)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
