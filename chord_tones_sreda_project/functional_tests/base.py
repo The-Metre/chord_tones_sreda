107d2f9ea75b2db7a578cdef68867a7be13c8edd
m_sreda <mitya.sreda@gmail.com>
2024-08-26 19:28:35 +0300

    changed ci file and functional test

--- chord_tones_sreda_project/functional_tests/base.py
+++ chord_tones_sreda_project/functional_tests/base.py
@@ -1,5 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+chrome_options.add_argument("--no-sandbox")  # Bypass OS security model, required for running in CI
+chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+
 browser = webdriver.Chrome()
 browser.get('http://localhost:8000')     
 
